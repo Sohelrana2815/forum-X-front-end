b31a789166b6a6b263172ee1683257c0ccbdf4c4
Sohelrana2815 <sohel152302@gmail.com>
2025-02-20 21:07:41 +0600

    done

--- src/Dashboard/CreatePost/CreatePost.jsx
+++ src/Dashboard/CreatePost/CreatePost.jsx
@@ -1,10 +1,100 @@
-
+import { useForm } from "react-hook-form";
+import { useAuth } from "../../Hooks/useAuth";
 const CreatePost = () => {
-    return (
+  const {
+    register,
+    handleSubmit,
+    // reset,
+    formState: { errors },
+  } = useForm();
+
+  const { user } = useAuth();
+  const onSubmit = async (data) => {
+    try {
+      // Prepare the post data
+
+      const postData = {
+        authorName: user.displayName,
+        authorEmail: user.email,
+        authorImage: user.photoURL,
+        title: data.title,
+        description: data.description,
+        tag: data.tag,
+        upVote: 0,
+        downVote: 0,
+        createdAt: new Date(),
+      };
+
+      console.log(postData);
+    } catch (error) {
+      console.error("Error creating post:", error);
+      alert("Failed to create post. Please try again.");
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2>Create a New Post</h2>
+
+        {/* Title */}
         <div>
-            Create Post
+          <label>Title</label>
+          <input
+            type="text"
+            {...register("title", { required: "Title is required." })}
+            placeholder="Enter post title"
+          />
+          {errors.title && (
+            <span className="text-[#C62300] text-base">
+              {errors.title.message}
+            </span>
+          )}
         </div>
-    );
+
+        {/* Description */}
+
+        <div className="form-group">
+          <label>Description</label>
+          <textarea
+            {...register("description", {
+              required: "Description is required",
+            })}
+            placeholder="Enter post description"
+          />
+          {errors.description && (
+            <span className="text-[#C62300] text-base">
+              {errors.description.message}
+            </span>
+          )}
+        </div>
+
+        {/* Tags */}
+
+        <div>
+          <label>Tag</label>
+          <select
+            defaultValue=""
+            {...register("tag", { required: "Tag is required" })}
+          >
+            <option value="">Select a tag</option>
+            <option value="technology">Technology</option>
+            <option value="web-development">Programming</option>
+            <option value="design">Design</option>
+          </select>
+          {errors && (
+            <span className="text-[#C62300] text-base">
+              {errors.tag?.message}
+            </span>
+          )}
+        </div>
+
+        <button type="submit" className="btn">
+          Submit
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default CreatePost;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
 import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
 import MainRoutes from "./Routes/MainRoutes/MainRoutes";
 import { AuthProvider } from "./Providers/AuthProvider/AuthProvider";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
-      <AuthProvider>
-        <MainRoutes />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <MainRoutes />
+        </AuthProvider>
+      </QueryClientProvider>
     </StrictMode>
   </BrowserRouter>
 );
