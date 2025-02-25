bed9128a39f74bc20659b5186b06712486954f82
Sohelrana2815 <sohel152302@gmail.com>
2025-02-25 10:56:16 +0600

    created ui for showing posts

--- src/Pages/Home/Posts/PostCard.jsx
+++ src/Pages/Home/Posts/PostCard.jsx
@@ -1,22 +1,45 @@
 const PostCard = ({ post }) => {
-  const { authorImage, title, tag, createdAt, authorName } = post;
+  const {
+    authorImage,
+    title,
+    tag,
+    createdAt,
+    authorName,
+    description,
+    upVote,
+    downVote,
+    voteDifference,
+  } = post;
   return (
     <>
-      <div className="border p-4 rounded-lg shadow-sm">
-        <div className="flex items-center space-x-4 mb-4">
-          <img
-            src={authorImage}
-            alt={authorName}
-            className="w-12 h-12 rounded-full"
-          />
+      <div className="card bg-base-100 shadow-xl">
+        <div className="card-body">
+          <div className="flex items-center gap-2 mb-4">
+            <img
+              src={authorImage}
+              alt={authorName}
+              className="w-14 h-14 rounded-full"
+            />
+            <div>
+              <h3 className="font-bold">{authorName}</h3>
+              <p className="text-sm text-gray-500">
+                {new Date(createdAt).toLocaleDateString()}
+              </p>
+            </div>
+          </div>
 
-          <h3 className="text-xl font-semibold mb-2">{title}</h3>
-          <div className="flex items-center space-x-4">
-            <span className="text-sm text-gray-500"># {tag}</span>
+          <h2 className="card-title">{title}</h2>
+          <p>{description}</p>
 
-            <span className="text-sm text-gray-500">
-              Posted on: {new Date(createdAt).toLocaleDateString()}
-            </span>
+          <div className="flex gap-4 mt-4">
+            <div className="badge badge-outline">{tag}</div>
+            <div className="flex items-center gap-1">
+              👍 {upVote}
+              👎 {downVote}
+              <span className="ml-2 font-bold">
+                (Total: {voteDifference})
+              </span>
+            </div>
           </div>
         </div>
       </div>
